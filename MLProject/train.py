89248a53cfceee0eb86bdf05e6d2fcb3a84f42f0
jonathanp5620 <jonathanp5620@gmail.com>
2025-12-15 00:22:18 +0700

    Update train.py

--- MLProject/train.py
+++ MLProject/train.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 import mlflow
 import mlflow.sklearn
 
@@ -13,11 +14,18 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.start_run()
-model = LinearRegression()
-model.fit(X_train, y_train)
+mlflow.set_experiment("Housing_CI_Experiment")
 
-mlflow.sklearn.log_model(model, "model")
-mlflow.end_run()
+with mlflow.start_run():
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+
+    mlflow.log_metric("MAE", mean_absolute_error(y_test, y_pred))
+    mlflow.log_metric("MSE", mean_squared_error(y_test, y_pred))
+    mlflow.log_metric("R2", r2_score(y_test, y_pred))
+
+    mlflow.sklearn.log_model(model, "model")
 
 print("Training selesai via CI")
