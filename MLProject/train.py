e3128d531707852baeefd32f0fa2f45ad47c1024
jonathanp5620 <jonathanp5620@gmail.com>
2025-12-15 00:37:01 +0700

    Load dataset and preprocess with one-hot encoding
    Load dataset and apply one-hot encoding to categorical columns.

--- MLProject/train.py
+++ MLProject/train.py
@@ -4,7 +4,11 @@ from sklearn.model_selection import train_test_split
 import mlflow
 import mlflow.sklearn
 
+# Load dataset
 df = pd.read_csv("Housing (1).csv")
+
+# One-hot encoding untuk kolom kategorikal
+df = pd.get_dummies(df, drop_first=True)
 
 X = df.drop("price", axis=1)
 y = df["price"]
