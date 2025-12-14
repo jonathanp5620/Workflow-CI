6268dfdb08f8a502ce79552a951a4fb4ec61f81f
jonathanp5620 <jonathanp5620@gmail.com>
2025-12-15 00:15:28 +0700

    Implement linear regression training with MLflow
    Added code for training a linear regression model using MLflow and scikit-learn.

--- MLProject/train.py
+++ MLProject/train.py
@@ -1,3 +1,23 @@
-entry_points:
-  main:
-    command: "python train.py"
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import mlflow
+import mlflow.sklearn
+
+df = pd.read_csv("Housing (1).csv")
+
+X = df.drop("price", axis=1)
+y = df["price"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+mlflow.start_run()
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+mlflow.sklearn.log_model(model, "model")
+mlflow.end_run()
+
+print("Training selesai via CI")
